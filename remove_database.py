57ee14046f5cb53a1e4bc9a0ddd1904212978792
cGIfl300 <cgifl300@gmail.com>
2022-08-08 21:01:08 +0200

    error interception when deleting the database

--- remove_database.py
+++ remove_database.py
@@ -2,4 +2,9 @@ import subprocess
 from pathlib import Path
 
 DB_PATH = str(Path.home()) + "\\stopme.json"
-result = subprocess.run(f"del {DB_PATH}")
+try:
+    result = subprocess.run(f"del {DB_PATH}")
+except FileNotFoundError:
+    print("Aucune base de donnée à supprimer")
+    exit()
+print("La base de données à été supprimée avec succès")
